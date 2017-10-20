ebaaffda7cda9b71a9b4b2df686b6b584a89201d
kirillyershov <dd041091eke@gmail.com>
2017-10-20 19:25:09 +0300

    Add validation, form template, some tests

--- boards/tests.py
+++ boards/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from .views import home, board_topics, new_topic
 from django.contrib.auth.models import User
 from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 class HomeTests(TestCase):
     def setUp(self):
@@ -94,6 +95,12 @@ class NewTopicTests(TestCase):
         self.assertTrue(Topic.objects.exists())
         self.assertTrue(Post.objects.exists())
 
+    def test_contains_form(self):  # <- new test
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, NewTopicForm)
+
     def test_new_topic_invalid_post_data(self):
         '''
         Invalid post data should not redirect
@@ -101,7 +108,9 @@ class NewTopicTests(TestCase):
         '''
         url = reverse('new_topic', kwargs={'pk': 1})
         response = self.client.post(url, {})
+        form = response.context.get('form')
         self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
 
     def test_new_topic_invalid_post_data_empty_fields(self):
         '''
